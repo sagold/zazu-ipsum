8634e329e4293d9db2bc51c5ffe4b63bd712d8cb
Sascha Goldhofer <post@saschagoldhofer.de>
2017-11-22 17:49:12 +0100

    Update

--- zazu/ipsum.js
+++ zazu/ipsum.js
@@ -8,7 +8,15 @@ module.exports = (pluginContext) => { // eslint-disable-line
      * @param  {String} query   - min max combination of values and type of words
      * @return {Array} list of ipsum versions
      */
-    return function (queryString) {
+    return function (queryString, env = {}) {
+        env = Object.assign({
+            paragraphs: 4,
+            sentenceMin: 8,
+            sentenceMax: 12,
+            wordsMin: 3,
+            wordsMax: 8
+        }, env);
+
         let pool;
         const query = queryString.trim()
             .split(" ")
@@ -30,26 +38,26 @@ module.exports = (pluginContext) => { // eslint-disable-line
             {
                 id: 0,
                 title: "words",
-                value: ipsum.words(query[0] || 5, query[1] || 12, pool)
+                value: ipsum.words(query[0] || env.wordsMin, query[1] || env.wordsMax, pool)
             },
             {
                 id: 1,
-                title: "sentence",
+                title: "a sentence",
                 value: ipsum.sentence(query[0] || 5, query[1] || 12, pool)
             },
             {
                 id: 2,
-                title: "paragraph",
+                title: "a paragraph",
                 value: ipsum.paragraph(query[0] || 8, query[1] || 12, query[2] || 3, query[3] || 8, pool)
             },
             {
                 id: 3,
-                title: "paragraphs",
+                title: `${query[0] || 4} paragraphs`,
                 value: ipsum.paragraphs(query[0] || 4, query[1] || 8, query[2] || 12, query[3] || 3, query[4] || 8, pool)
             },
             {
                 id: 4,
-                title: "html paragraphs",
+                title: `${query[0] || 4} html paragraphs`,
                 value: ipsum.htmlParagraphs(query[0] || 4, query[1] || 8, query[2] || 12, query[3] || 3, query[4] || 8, pool)
             }
         ]);
